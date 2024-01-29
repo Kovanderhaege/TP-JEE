57ee03ceedc8199c47e9b29403972fa3a6b3f445
Adrien Mareel <mareel.adri@yahoo.com>
2024-01-29 11:26:03 +0100

    Delete movie feature

--- lab05-web/src/main/java/junia/lab05/web/controller/MoviesController.java
+++ lab05-web/src/main/java/junia/lab05/web/controller/MoviesController.java
@@ -13,7 +13,6 @@ public class MoviesController {
     final private MovieService movieService;
     final private PhaseService phaseService;
 
-
     public MoviesController(MovieService movieService, PhaseService phaseService) {
         this.movieService = movieService;
         this.phaseService = phaseService;
@@ -30,5 +29,11 @@ public class MoviesController {
     public String saveMovie(@ModelAttribute("movie") Movie movie, @RequestParam("phase") long id){
         movieService.saveMovieInPhase(movie, id);
         return "redirect:../sagas/list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteMovie(@PathVariable("id") long movieId){
+        movieService.deleteById(movieId);
+        return "redirect:../../sagas/list";
     }
 }
